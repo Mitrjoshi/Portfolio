a1c5014e29e103507101186e2b89fb1f479f9112
Mitrjoshi <mitrjoshi26@gmail.com>
2024-12-14 19:44:35 +0530

    1

--- src/Root.tsx
+++ src/Root.tsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 
 export const Root = ({ children }: PropsWithChildren) => {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <div className="container relative mx-auto scroll-my-12 overflow-auto px-4 pt-6 md:p-x-16 md:pt-16">
         {children}
         <Outlet />
